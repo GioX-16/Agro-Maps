951e56bb5f205fd8ac094d0f9c76b2e74e7d43fd
Wilhelm Reyes <wilhelmeselpro@gmail.com>
2024-09-02 10:59:52 -0600

    Creado la estructura inicial para la aplicacion web La tecnologia elegida fue django por su faciliad a la hora de integrar la tecnologia de blockchain

---
+++ agroMaps/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for agroMaps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroMaps.settings')
+
+application = get_wsgi_application()
---
+++ loginFeature/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoginfeatureConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'loginFeature'
